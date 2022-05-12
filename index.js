9ebbb9529102ee92ec599589421f238571773b51
Asura-Y <fabiocosentino7@gmail.com>
2022-05-12 11:52:30 +0200

    Implemented changing port feature from external text file

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const http = require('http');
 const os = require('os');
-const port = 3001;
+const port = require('./port.json');
 const ip = os.networkInterfaces().lo[0].address;
 
 const server = http.createServer((req, res) => {
     console.log("New Request Received");
 });
 
-server.listen(port, ip, () => {
-    console.log(`Server Listening on ${ip}:${port}`);
+server.listen(port.port, ip, () => {
+    console.log(`Server Listening on ${ip}:${port.port}`);
 });
 
 server.on('error', err => console.log(err));
