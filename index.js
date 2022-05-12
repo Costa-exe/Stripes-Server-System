5fe2123ebf87dd3e35e033510819f92af848f219
Asura-Y <fabiocosentino7@gmail.com>
2022-05-12 14:35:54 +0200

    added html page as server response, also added storage folder for future tests

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const http = require('http');
 const os = require('os');
+const fs = require('fs');
+const html = fs.readFileSync('./fm.html');
 const port = require('./port.json');
 const ip = os.networkInterfaces().lo[0].address;
 
 const server = http.createServer((req, res) => {
     console.log("New Request Received");
+    res.statusCode = 200;
+    res.setHeader('Content-type', 'html');
+    res.write(html);
+    res.end();
 });
 
 server.listen(port.port, ip, () => {
