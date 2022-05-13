d8455467d6eec5a1df26685b34e94ae33f0ed2eb
Asura-Y <fabiocosentino7@gmail.com>
2022-05-13 10:33:14 +0200

    Final Testing Version - server is now able to check the storage folder and list folders and files on console

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require('http');
 const os = require('os');
 const fs = require('fs');
+const storage = './stored/';
 const html = fs.readFileSync('./fm.html');
 const port = require('./port.json');
 const ip = os.networkInterfaces().lo[0].address;
@@ -8,6 +9,9 @@ const ip = os.networkInterfaces().lo[0].address;
 const server = http.createServer((req, res) => {
     console.log("New Request Received");
     res.statusCode = 200;
+    fs.readdirSync(storage).forEach(file => {
+        console.log(file);
+    });
     res.setHeader('Content-type', 'html');
     res.write(html);
     res.end();
